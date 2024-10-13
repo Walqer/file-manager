6fb524582c591ace567046ff86c93157059afb7e
Alimusim <alimossim.agayev@fdrive.kz>
2024-10-13 11:28:05 +0500

    Revert "split to modules"
    This reverts commit 40378e910721e7424512b04e5c0d13a779110d13.

--- src/helpers/changeDirectory.js
+++
@@ -1,8 +0,0 @@
-export async function changeDirectory(currentPath, newPath,state = null) {
-    const newPathResolved = path.resolve(currentPath, newPath);
-    await fs.access(newPathResolved);
-    process.chdir(parentPath);
-    if(state){
-        state = newPathResolved
-    }
-}
